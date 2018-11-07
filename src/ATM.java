7cf3bc37aa0ca7cd8520e79549375774d945081f
tegenton <tegenton@ucvts.tec.nj.us>
2018-11-07 15:07:48 +0000

    Finish account creator

--- src/ATM.java
+++ src/ATM.java
@@ -5,30 +5,33 @@ public class ATM {
     ATM(BankAccount currentAccount) {
         this.currentAccount = currentAccount;
     }
-    
-    Scanner in = new Scanner(System.in);
-    
+
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-    	
-    	
+        ATM atm = new ATM(createAccount());
     }
-    
-    public BankAccount createAccount() {
-    	String name; int ssn; String phone; String address;
-    	System.out.println("Enter your information to create an account");
-    	System.out.print("Name:");
-    	name = in.nextLine();
-    	System.out.print("SSN:");
-    	ssn = in.nextInt();
-    	System.out.print("Phone:");
-    	phone = in.nextLine();
-    	System.out.println("Address:");
-    	address = in.nextLine();
-    	
-    	AccountHolder user = new AccountHolder(name, ssn, phone, address);
-    	
-    	System.out.print("Pin:");
-    	int pin = in.nextInt();
-    	
+
+    public static BankAccount createAccount() {
+        String name; int ssn; String phone; String address;
+        System.out.println("Enter your information to create an account");
+        System.out.print("Name: ");
+        name = in.nextLine();
+        System.out.print("SSN: ");
+        ssn = in.nextInt();
+        in.nextLine();
+        System.out.print("Phone: ");
+        phone = in.nextLine();
+        System.out.print("Address: ");
+        address = in.nextLine();
+
+        AccountHolder user = new AccountHolder(name, ssn, phone, address);
+
+        System.out.print("Pin: ");
+        int pin = in.nextInt();
+        System.out.print("Account Number: ");
+        int accountNumber = in.nextInt();
+
+        return new BankAccount(user, pin, accountNumber);
     }
 }   
